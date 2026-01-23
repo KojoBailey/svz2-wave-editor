77debd43dfc542e9946101de3ad459eea1271dd9
Kojo Bailey <kojomolojo@gmail.com>
2026-01-23 20:08:35 +0000

    Make buttons dynamic

--- scripts/draggable.gd
+++ scripts/draggable.gd
@@ -1,35 +1,40 @@
 extends Area2D
 
-var isDragging = false
+## Interface
+
+func set_texture(texture: Texture2D):
+	$Sprite2D.texture = texture
+
+## Implementation
+
+var is_dragging = false
 var offset: float = 0
 
-var spriteHalfSize: Vector2i
-
-var leftBound: float
-var rightBound: float
+var left_bound: float
+var right_bound: float
 
 func _ready() -> void:
-	spriteHalfSize = $Sprite2D.texture.get_size() * self.scale / 2
-	leftBound = spriteHalfSize.x
-	rightBound = get_viewport_rect().size.x - spriteHalfSize.x
+	var sprite_half_size = $Sprite2D.texture.get_size() * self.scale / 2
+	left_bound = sprite_half_size.x
+	right_bound = get_viewport_rect().size.x - sprite_half_size.x
 	
-	global_position = Vector2(rightBound, get_viewport_rect().size.y - spriteHalfSize.y)
+	self.global_position = Vector2(right_bound, get_viewport_rect().size.y - sprite_half_size.y)
 
 func _process(_delta: float) -> void:
-	if isDragging:
-		var targetX = get_global_mouse_position().x - offset
-		global_position.x = clamp(targetX, leftBound, rightBound)
+	if is_dragging:
+		var target_x = get_global_mouse_position().x - offset
+		self.global_position.x = clamp(target_x, left_bound, right_bound)
 		
-	#var ratio = inverse_lerp(leftBound, rightBound, self.position.x)
+	#var ratio = inverse_lerp(left_bound, right_bound, self.position.x)
 
 func _input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
-			isDragging = true
+			is_dragging = true
 			offset = get_global_mouse_position().x - global_position.x
 		else:
-			isDragging = false
+			is_dragging = false
 
 func _input(event: InputEvent):
 	if event is InputEventMouseButton and not event.pressed:
-		isDragging = false
+		is_dragging = false
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,13 +1,23 @@
 extends Node2D
 
-var draggableScene = preload("res://scenes/draggable.tscn")
+@export var enemy_list: Array[Texture2D]
+
+var enemy_button_scene = preload("res://scenes/enemy_button.tscn")
+var draggable_scene = preload("res://scenes/draggable.tscn")
 
 func _ready() -> void:
-	pass
+	for i in range(enemy_list.size() - 1):
+		create_button(enemy_list[i], i)
+	
+func create_button(texture: Texture2D, index: int) -> void:
+	var enemy_button = enemy_button_scene.instantiate() as TextureButton
+	enemy_button.texture_normal = texture
+	enemy_button.position.x = index * (enemy_button.size.x + 40)
+	enemy_button.button_up.connect(_on_enemy_button_up.bind(enemy_button))
+	
+	self.add_child(enemy_button)
 
-func _process(delta: float) -> void:
-	pass
-
-func _on_texture_button_button_up() -> void:
-	var draggable = draggableScene.instantiate()
+func _on_enemy_button_up(button: TextureButton) -> void:
+	var draggable = draggable_scene.instantiate()
+	draggable.set_texture(button.texture_normal) as Node2D
 	self.add_child(draggable)
